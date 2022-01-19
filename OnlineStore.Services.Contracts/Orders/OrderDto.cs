6c16d669d9ab79f5ef27f06d456f18b446e146e7
MikhailMamonov <mamon201071@gmail.com>
2022-01-19 18:54:37 +0300

    feat (Services, Infrastructure): begin work with Application layer

---
+++ OnlineStore.Services.Contracts/Orders/OrderDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OnlineStore.Services.Contracts
+{
+    public class OrderDto
+    {
+        public string ZipCode { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public decimal OrderTotal { get; set; }
+
+        public DateTime OrderPlaced { get; set; }
+    }
+}
---
+++ OnlineStore.Services.Interfaces/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineStore.Services.Interfaces
+{
+    public interface IProductService
+    {
+
+    }
+}
