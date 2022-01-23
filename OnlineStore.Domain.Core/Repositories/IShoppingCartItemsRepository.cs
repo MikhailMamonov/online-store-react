71762ff98c54d5619e4d8006905bddcddc4c6b28
MikhailMamonov <mamon201071@gmail.com>
2022-01-23 15:28:50 +0300

    feat (Domain, Infrastructure, services): Start Work with ShoppingCart Feature

---
+++ OnlineStore.Domain.Core/Repositories/IShoppingCartItemsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineStore.Domain.Core.Entities;
+using OnlineStore.Domain.Core.Repositories.Base;
+
+namespace OnlineStore.Domain.Core.Repositories
+{
+    public interface IShoppingCartItemsRepository: IRepository<CartItem>
+    {
+    }
+}
